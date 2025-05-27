43890ddff2418d4234ec0b81c4cde44e5faf1231
Mahesh Mhetre <mhetrem15@gmail.com>
2025-05-28 00:00:01 +0530

    adding reports

--- src/main/java/Framework/testCases/SignupTest.java
+++ src/main/java/Framework/testCases/SignupTest.java
@@ -3,6 +3,7 @@ package Framework.testCases;
 import Framework.Functions.common;
 import Framework.Functions.screenShot;
 import Framework.Functions.signUp;
+import io.qameta.allure.*;
 import org.openqa.selenium.WebDriver;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
@@ -31,7 +32,12 @@ public class SignupTest {
 
     }
 
-    @Test
+    @Test(priority = 1, description = "Duplicate User Registration Test")
+    @Description("This test case checks the scenario where a user tries to register with an email that is already registered.")
+    @Epic("User Registration")
+    @Feature("Feature1: Registration")
+    @Story("Story1: Duplicate Registration")
+    @Severity(SeverityLevel.BLOCKER)
     public void TC002_DuplicateUserRegistration() throws Exception{
         log().info("====TC002 Duplicate User Registration started====");
         si.DuplicateUser();
@@ -39,7 +45,12 @@ public class SignupTest {
 
     }
 
-    @Test
+    @Test(priority = 2, description = "Registration with Empty Required Fields Test")
+    @Description("This test case checks the scenario where a user tries to register without filling in the required fields.")
+    @Epic("User Registration")
+    @Feature("Feature1: Registration")
+    @Story("Story2: Empty Required Fields")
+    @Severity(SeverityLevel.CRITICAL)
     public void TC003_RegistrationWithEmptyRequiredFields() throws Exception{
         log().info("====TC003 Registration with Empty Required Fields started====");
         si.checkSignupReqFields();
@@ -47,7 +58,12 @@ public class SignupTest {
 
     }
 
-    @Test
+    @Test(priority = 3, description = "Registration with Invalid Email Test")
+    @Description("This test case checks the scenario where a user tries to register with an invalid email format.")
+    @Epic("User Registration")
+    @Feature("Feature1: Registration")
+    @Story("Story3: Invalid Email Registration")
+    @Severity(SeverityLevel.NORMAL)
     public void TC004_RegisterWithInvalidEmail() throws Exception{
         log().info("====TC004 Registration with Invalid Email Fields started====");
         si.invalidEmailRegistration();
